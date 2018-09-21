bf1c121dcdbe1a9249045fb91c2ff336ff1ba4ef
Admin <Admin@DESKTOP-LK0OECV>
2018-09-20 22:34:54 -0300

    Lambda de search

--- iwe-avengers-search/src/main/java/com/iwe/avengers/SearchAvengersHandler.java
+++ iwe-avengers-search/src/main/java/com/iwe/avengers/SearchAvengersHandler.java
@@ -7,8 +7,19 @@ import com.iwe.avenger.lambda.response.HandlerResponse;
 
 public class SearchAvengersHandler implements RequestHandler<Avenger, HandlerResponse> {
 
+	private AvenderDAO dao = new AvenderDAO();
+	
 	@Override
 	public HandlerResponse handleRequest(final Avenger avenger, final Context context) {
-		return null;
+		
+		context.getLogger().log("[#] - Searching avenger by id: " + avenger.getId());
+		
+		final Avenger avengerFound = dao.search(avenger.getId());
+		
+		final HandlerResponse response = HandlerResponse.builder().setObjectBody(avengerFound).build();
+		
+		context.getLogger().log("[#] - Avenger found!");
+		
+		return response;
 	}
 }
